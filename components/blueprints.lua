76d984d7de2e807344edfe37fdb5cc3c52d13216
Henk Boom <henk@henk.ca>
2010-04-09 16:39:09 -0400

    removed obsolete collider arguments

--- components/blueprints.lua
+++ components/blueprints.lua
@@ -5,18 +5,18 @@ local v2 = require 'dokidoki.v2'
 player_ship = game.make_blueprint('player_ship',
   {'transform'},
   {'sprite', resource='ship_sprite'},
-  {'collider', class='ship', poly=collision.make_rectangle(12, 8)},
+  {'collider', poly=collision.make_rectangle(12, 8)},
   {'player_ship_control'},
   {'ship'},
   {'real_ship'})
 
 obstacle = game.make_blueprint('obstacle',
   {'transform'},
-  {'collider', class='obstacle'},
+  {'collider'},
   {'collider_renderer', color={0.7, 0.5, 0.5}})
 
 checkpoint = game.make_blueprint('checkpoint',
   {'transform'},
-  {'collider', class='checkpoint'},
+  {'collider'},
   {'collider_renderer', color={0, 0, 1, 0.1}},
   {'checkpoint'})
