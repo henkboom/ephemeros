e36d888026061c9c1862ef47c14ffb2fb85311e7
Henk Boom <henk@henk.ca>
2010-04-16 23:26:43 -0400

    use henk.ca as server, added error handling for network access

--- components/game_flow.lua
+++ components/game_flow.lua
@@ -1,5 +1,7 @@
 local v2 = require 'dokidoki.v2'
 
+local BASE_URL = 'http://stuff.henk.ca/dart450/api/'
+
 local GHOST_OFFSET_TIME = 20
 local TOTAL_TIME = 60 * 20
 local INITIAL_STATE = {
@@ -20,22 +22,63 @@ game.actors.new_generic('game_flow', function ()
 
   local ghosts = {}
   local parent_id
+  local ghost_to_send
 
-  print 'loading ghosts...'
-  local data = http.request('http://localhost/miniracer/?get-race')
-  local _, ending
-  _, ending, parent_id = data:find('^([0-9]*):')
-  local start = ending + 1
-  while start <= #data do
-    local recording
-    recording, start = ghost.deserialize(INITIAL_STATE, data, start)
-    table.insert(ghosts, recording)
+  local function get_ghosts()
+    print 'loading ghosts...'
+
+    local data, code = http.request(BASE_URL .. '?get-race')
+    if not data then
+      print('network error: ' .. code)
+      return false
+    elseif code ~= 200 then
+      print('recieved http response code: ' .. code)
+      return false
+    end
+
+    local _, ending
+    _, ending, parent_id = data:find('^([0-9]*):')
+    local start = ending + 1
+    while start <= #data do
+      local recording
+      recording, start = ghost.deserialize(INITIAL_STATE, data, start)
+      table.insert(ghosts, recording)
+    end
+
+    print('loaded ' .. #ghosts .. ' ghosts')
+    return true
   end
-  print('loaded ' .. #ghosts .. ' ghosts')
+
+  local function send_ghost()
+    if not ghost_data_to_send then
+      local player = game.actors.get('player_ship')[1]
+      ghost_data_to_send = parent_id .. ':'
+                           .. player.recorder.cut_recording().serialize()
+    end
+    print('sending player ghost...')
+
+    local response, code = http.request(BASE_URL, ghost_data_to_send)
+    if not response then
+      print('network error: ' .. code)
+      return false
+    elseif code ~= 200 then
+      print('recieved http response code: ' .. code)
+      return false
+    end
+
+    print('done sending')
+    return true
+  end
 
   local time = 0
 
   function update()
+    if time == 0 then
+      if not get_ghosts() then
+        return -- try again next frame
+      end
+    end
+
     if time % GHOST_OFFSET_TIME == 0 then
       local index = time / GHOST_OFFSET_TIME
       if ghosts[index] then
@@ -49,14 +92,12 @@ game.actors.new_generic('game_flow', function ()
       player.ship.set_state(INITIAL_STATE)
     end
     if time == TOTAL_TIME then
-      local player = game.actors.get('player_ship')[1]
-      local data = parent_id .. ':'
-                   .. player.recorder.cut_recording().serialize()
-      print('sending player ghost...')
-      print(http.request('http://localhost/miniracer/', data))
-      print('done sending')
+      if not send_ghost() then
+        return
+      end
       kernel.switch_scene(race.make())
     end
+
     time = time + 1
   end
 
